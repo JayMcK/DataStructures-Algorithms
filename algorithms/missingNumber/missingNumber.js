b5c683245c2890aeac16f20f387c806b8b53a77d
Jay McKenzie <jay_mckenzie1@outlook.com>
2021-10-25 15:49:28 +0100

    6. missingNumber - HashTable Pattern

---
+++ algorithms/missingNumber/missingNumber.js
@@ -0,0 +1,41 @@
+{
+  /* 
+          PROBLEM:
+          Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+          
+          EXAMPLES:
+          Input: nums = [1,2,3,1]
+          Output: true
+      
+          Input: nums = [3,0,1]
+          Output: 2
+          Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+          */
+}
+
+{
+  /*
+            PSUEDOCODE:
+            1. create hashtable. populate with data from nums array input.
+            2. loop over hashtable, as many times as length of nums array, as this is the range. So that we visit each value from 0.
+            3. If any num doesn't exist in the hashtable, this is the missing value. Return it.
+            */
+}
+
+//TIME COMPLEXITY = 0(n) - LINEAR TIME
+//SPACE COMPLEXITY = - 0(n) - LINEAR SPACE
+
+//SOLUTION
+function missingNumber(nums) {
+  const lookup = {};
+
+  for (let num of nums) {
+    lookup[num] = 1;
+  }
+
+  for (let i = 0; i <= nums.length; i++) {
+    if (!lookup[i]) return i;
+  }
+}
+
+console.log(missingNumber([0, 1]));
